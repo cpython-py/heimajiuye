8699be796ec9d7e0f5f99f9092d1e181ef8ee67b
cpython-py <zero888_666@qq.com>
2026-04-24 19:16:58 +0800

    ``` feat(auth): 添加登录注册功能和公共提示组件
    - 在common.js中添加了公共的提示框函数showToast
    - 实现了登录页面的表单验证、API调用和本地存储功能
    - 实现了注册页面的数据收集、校验和跳转逻辑
    - 修复了侧边栏HTML结构问题，确保导航正常显示
    ```

---
+++ 黑马就业数据平台-基础模板/js/login.js
@@ -0,0 +1,26 @@
+const loginForm = document.querySelector('.login-form')
+// 1. NOTE - 注册事件
+document.querySelector('#btn-login').addEventListener('click', async e => {
+    // 2. NOTE - 收集表单数据并验证
+    const data = serialize(loginForm, { hash: true, empty: true })
+    if (!data.username || !data.password) return showToast('请输入用户名或密码')
+    if (data.username.length < 8 || data.username.length > 30) return showToast('用户名长度必须在8-30之间')
+    if (data.password.length < 6 || data.password.length > 30) return showToast('密码长度必须在6-30之间')
+    
+    // 3.发送请求,判断响应结果,提示用户
+    try {
+        const res = await axios.post('/login', data)
+        console.log(res)
+        const obj = {}
+        obj.usernam = res.data.data.username
+        // 4.如果成功,则本地存储用户名
+        localStorage.setItem('userMsg', JSON.stringify(obj))
+        showToast(res.data.message)
+        // 5.跳转页面
+        setTimeout(() => {
+            location.href = './index.html'
+        }, 1500)
+    }
+    catch (err) { showToast(err.response.data.message) }
+    
+})

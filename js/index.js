842897a9ff3db36e49ed551bd9aea877554a93a0
cpython-py <zero888_666@qq.com>
2026-04-25 22:10:59 +0800

    ``` feat(js): 添加数据统计功能并优化图表展示
    新增获取统计数据功能，实现动态数据渲染到图表中。
    优化图表样式包括：添加坐标轴线、渐变色线条、阴影区域、虚线轴线等视觉效果。
    ```

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ checkToken(); // NOTE - 验证token
 renderUserName(); // NOTE - 回显用户名
 logout(); // NOTE - 退出登录
 
+// NOTE - 获取统计数据
 const getData = async () => {
         const res = await axios({
             url: "/dashboard",
@@ -34,9 +35,18 @@ const renderYear = year => {
         grid: {
             top: '15%',
         },
+        tooltip: {
+            trigger: 'axis',
+        },
         xAxis: {
             type: 'category',
-            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+            data: year.map(item => item.month),
+            axisLine: {
+                lineStyle: {
+                    type: 'dashed',
+                    color: '#ccc'
+                }
+            }
         },
         yAxis: {
             type: 'value',
@@ -48,10 +58,37 @@ const renderYear = year => {
         },
         series: [
             {
-                data: [820, 932, 901, 934, 1290, 1330, 1320],
+                data: year.map(item => item.salary),
                 type: 'line',
+                symbolSize: 10,
+                lineStyle: {
+                    width: 10,
+                    color: {
+                        type: 'linear',
+                        x: 0,
+                        y: 0,
+                        x2: 1,
+                        y2: 0,
+                        colorStops: [
+                            { offset: 0, color: '#479dee' },
+                            { offset: 1, color: '#5c75f0' }
+                        ]
+                    }
+                },
                 smooth: true,
-                areaStyle: {}
+                areaStyle: {
+                    color: {
+                        type: 'linear',
+                        x: 0,
+                        y: 0,
+                        x2: 0,
+                        y2: 1,
+                        colorStops: [
+                            { offset: 0, color: '#a9d2f7' },
+                            { offset: 1, color: 'rgba(255, 255, 255, 0)' }
+                        ]
+                    }
+                }
             }
         ]
     };

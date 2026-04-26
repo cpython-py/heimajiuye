7ca8657c7bc1e4f17f57dea29a2f569ab370b397
cpython-py <zero888_666@qq.com>
2026-04-26 11:14:07 +0800

    ``` feat(student): 添加学生列表渲染功能
    新增render函数实现学生数据的获取和表格展示，
    包括学生的基本信息如姓名、年龄、性别、组别、期望薪资等，
    并添加了编辑和删除按钮用于后续操作。
    ```

--- js/student.js
+++ js/student.js
@@ -1,3 +1,27 @@
 checkToken() // NOTE - 登录校验
 renderUserName() // NOTE - 渲染用户名
 logout() // NOTE - 退出登录
+
+// NOTE - 渲染业务
+const render = async () => {
+    const { data } = await axios.get('/students', { params: {} })
+    document.querySelector('.list').innerHTML = data.map(item => {
+        const { age, area, city, gender, group, hope_salary, salary, name, province } = item
+        return `
+            <tr>
+                <td>${name}</td>
+                <td>${age}</td>
+                <td>${gender?'女':'男'}</td>
+                <td>${group}</td>
+                <td>${hope_salary}</td>
+                <td>${salary}</td>
+                <td>${province}${city}${area}</td>
+                <td>
+                    <a href="javascript:;" class="text-success mr-3"><i class="bi bi-pen"></i></a>
+                    <a href="javascript:;" class="text-danger"><i class="bi bi-trash"></i></a>
+                </td>
+            </tr>
+        `
+    }).join('')
+}
+render()
